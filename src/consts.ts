b9dba99837f8b0c1403cbddccd7284d4a2110927
qiuyixiang <qiuyixiang2003@gmail.com>
2025-09-29 13:42:00 -0500

    setup theme template for astro

---
+++ src/consts.ts
@@ -0,0 +1,41 @@
+// Place any global data in this file.
+// You can import this data from anywhere in your site by using the `import` keyword.
+
+export const SITE_TITLE = "My Blog";
+export const SITE_DESCRIPTION =
+    "A personal blog about technology, thoughts, and experiences";
+
+// If you deploy to a subdirectory, set the `WEBSITE_BASE` here.
+// e.g. '/blog' if you are deploying to example.com/blog
+// Leave it as an empty string if you are deploying to a root domain (e.g. example.com)
+export const SITE_BASE = "";
+
+export const SocialLinks = [
+    {
+        name: "GitHub",
+        url: "https://github.com/yourusername",
+    },
+    {
+        name: "LinkedIn",
+        url: "https://www.linkedin.com/in/yourprofile",
+    },
+    {
+        name: "X / Twitter",
+        url: "https://twitter.com/yourusername",
+    },
+];
+
+export const WebsiteLinks = [
+    {
+        name: "Home",
+        url: "",
+    },
+    {
+        name: "Blog",
+        url: "blog",
+    },
+    {
+        name: "About",
+        url: "about",
+    },
+];
